5fbf0a665d64f0e77f44a6381b2a2d9781cf1c54
sila-kizilirmak <silakzlrmk03@gmail.com>
2025-06-12 22:54:27 +0200

    test playername

--- src/test/java/ProvidePlayerNameTest.java
+++ src/test/java/ProvidePlayerNameTest.java
@@ -1,15 +1,15 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
-// import java.io.ByteArrayInputStream;
-// import java.util.Scanner;
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
 
-// public class ProvidePlayerNameTest {
+public class ProvidePlayerNameTest {
 
-//     @Test
-//     public void testProvidePlayerName_ValidName() {
-//         String input = "Alice\n";
-//         Scanner scanner = new Scanner(new ByteArrayInputStream(input.getBytes()));
-//         String result = Main.providePlayerName(scanner);
-//         assertEquals("Alice", result);
-//     }}
+    @Test
+    public void testProvidePlayerName_ValidName() {
+        String input = "Alice\n";
+        Scanner scanner = new Scanner(new ByteArrayInputStream(input.getBytes()));
+        String result = Main.providePlayerName(scanner);
+        assertEquals("Alice", result);
+    }}
